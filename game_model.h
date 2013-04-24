678343b1ce088e8285725d0e4c2d0f8978fc5d57
MaTachi <dannie92@gmail.com>
2013-04-24 16:51:05 +0200

    Update formatting in game_model.h

--- game_model.h
+++ game_model.h
@@ -21,23 +21,32 @@ class GameModel {
     static const int BLOCKS_IN_ROW = 10;
     static const int BLOCKS_IN_COL = 22;
   private:
-    bool collides_with_grid(std::vector<Block*>, std::vector<Block*>);
-    void move(std::vector<Block*>, Direction);
-    void rotate_figure(std::vector<Block*>&);
+    // Check if the figure collides with the blocks
+    bool collides_with_grid(std::vector<Block*> figure,
+        std::vector<Block*> blocks);
+    // Move figure in given direction
+    void move(std::vector<Block*> figure, Direction direction);
+    // Rotate the figure 1 step clockwise
+    void rotate_figure(std::vector<Block*>& figure);
+    // Create a figure, returns a refernce to a new figure
     std::vector<Block*>* create_figure(std::vector<Block*>* blocks);
+    // Check and clear all full rows
     void delete_full_rows(std::vector<Block*>& blocks);
+    // Move down all blocks above given level 1 step
     void move_down_blocks_above_level(std::vector<Block*>& blocks, int level);
-
     // Create a new block and add it to a figure and the collection of all
     // blocks.
-    void create_block(int x, int y, GLcolor color, std::vector<Block*>* blocks, std::vector<Block*>* figure);
+    void create_block(int x, int y, GLcolor color, std::vector<Block*>* blocks,
+        std::vector<Block*>* figure);
 
+    // Collection of all blocks
     std::vector<Block*> blocks_;
 
-    //float clock_;
+    // When the figure should move down 1 step
     Uint32 next_tick_;
+
+    // The figure, i.e. the moving block formation
     std::vector<Block*> figure_;
-    //std::vector<Block*>* figure2_;
 };
 
 #endif
